8c1c038c69be80576ef075a091a672449bcfb998
SUPR1M3 <surajsingh.041@iiitb.org>
2024-08-03 12:45:15 -0400

    Added Space-Invaders

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -3,6 +3,7 @@ import styles from './ProjectsStyles.module.css';
 import covfefe from '../../assets/Covfefe.png';
 import PortFolio from '../../assets/Favicon.png';
 import BookLetter from '../../assets/BookLetterIcon.png'
+import SpaceInvaders from '../../assets/SpaceShip.png'
 import ProjectCard from '../../common/ProjectCard.jsx';
 
 function Projects() {
@@ -12,6 +13,7 @@ function Projects() {
         <div className={styles.projectsContainer}>
             <ProjectCard icon = {covfefe} link ="https://github.com/SUPR1M3/Covfefe" heading="Covfefe" desc="Zomato for Cafes"/>
             <ProjectCard icon = {BookLetter} link ="https://github.com/SUPR1M3/BookLetter" heading="BookLetter" desc="Library Database"/>
+            <ProjectCard icon = {SpaceInvaders} link ="https://github.com/SUPR1M3/Space-Invaders" heading="Space-Invaders" desc="Classic Video Game"/>
             <ProjectCard icon = {PortFolio} link ="https://github.com/SUPR1M3/react-portfolio" heading="PortFolio" desc="Professional Portfolio"/>
         </div>
     </section>
