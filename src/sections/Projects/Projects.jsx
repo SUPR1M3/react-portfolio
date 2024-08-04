9c5bec3701b1dfb168925350965370d64c59a8e2
SUPR1M3 <surajsingh.041@iiitb.org>
2024-08-04 07:20:48 -0400

    Added Reel-Good

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import styles from './ProjectsStyles.module.css';
 import covfefe from '../../assets/Covfefe.png';
 import PortFolio from '../../assets/Favicon.png';
-import BookLetter from '../../assets/BookLetterIcon.png'
-import SpaceInvaders from '../../assets/SpaceShip.png'
+import BookLetter from '../../assets/BookLetterIcon.png';
+import SpaceInvaders from '../../assets/SpaceShip.png';
+import ReelGood from '../../assets/ReelGoodIcon.png';
 import ProjectCard from '../../common/ProjectCard.jsx';
 
 function Projects() {
@@ -14,6 +15,7 @@ function Projects() {
             <ProjectCard icon = {covfefe} link ="https://github.com/SUPR1M3/Covfefe" heading="Covfefe" desc="Zomato for Cafes"/>
             <ProjectCard icon = {BookLetter} link ="https://github.com/SUPR1M3/BookLetter" heading="BookLetter" desc="Library Database"/>
             <ProjectCard icon = {SpaceInvaders} link ="https://github.com/SUPR1M3/Space-Invaders" heading="Space-Invaders" desc="Classic Video Game"/>
+            <ProjectCard icon = {ReelGood} link ="https://github.com/SUPR1M3/Reel-Good" heading="Reel-Good" desc="IMDB clone"/>
             <ProjectCard icon = {PortFolio} link ="https://github.com/SUPR1M3/react-portfolio" heading="PortFolio" desc="Professional Portfolio"/>
         </div>
     </section>
